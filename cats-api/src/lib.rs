68200b9d13360cea379ae6d3cea7adb16da6e0d3
Chiro Liang <Chiro2001@163.com>
2022-12-30 20:07:32 +0800

    simple backend

--- cats-api/src/lib.rs
+++ cats-api/src/lib.rs
@@ -1,6 +1,8 @@
 pub mod user;
 pub mod jwt;
 
+pub const PORT: u16 = 8080;
+
 #[cfg(test)]
 mod tests {
     use super::*;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+use warp::Filter;
 use cats_api::*;
 
-fn main() {
-    println!("Hello, world!");
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    println!("school-cats backend! server running on http://127.0.0.1:{}/", PORT);
+    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}", name));
+    warp::serve(hello).run(([127, 0, 0, 1], PORT)).await
 }
